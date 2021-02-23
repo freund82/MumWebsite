87320d360f8197df681672f0049a1bacb403206e
Maxim Utkin <maxim2001_2001@mail.ru>
2021-02-23 23:24:56 +0300

    Получилось с отправкой данных

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,4 @@
 $('#order').submit(function(){
     var formData=$('#order').serialize();
-    $.get('order.php', formData, result);
-    function result(data){
-        alert(data);
-        /*if (data === 'pass'){
-            alert("Данные переданы усешно");
-        } else {
-            alert("Упс:(");
-        }*/
-    } // Конец result
+    $.post('order.php', formData);
 }); // конец submit
--- order.php
+++ order.php
@@ -1,15 +1,27 @@
 <?php
-$name="maxim";
+//получаем данные в массив POST
+$name=$_POST['name'];
+$tel=$_POST['tel'];
+$email=$_POST['email'];
+
+//проверяем переменные на получение вредоносных символов
+$name=htmlspecialchars($name);
+$tel=htmlspecialchars($tel);
+$email=htmlspecialchars($email);
+
+//декодируем url если кто-то попытается ввесди имя сайта в форму
+$name=urldecode($name);
+$tel=urldecode($tel);
+$email=urldecode($email);
+
+//удаляем пробеллы если есть в начале и конца строки если есть
+$name=trim($name);
+$tel=trim($tel);
+$email=trim($email);
 
 
-$na=isset($_GET['name']) ? $_GET['name'] : $_POST['name'];
-
-
-
-if ($na==$name) {
-  echo 'pass';
-} else {
-  echo 'fail';
-}
-
+// mail("maximutkin82@yandex.ru",  "Заявка с сайта",  "ФИО ".$name.".  tel: ".$tel.".  email: ".$email , "From: webhobby@maxhooktravelblog.ru \r\n");
+mail("maximutkin82@yandex.ru",  "Заявка с сайта",  "ФИО ".$name.".  tel: ".$tel.".  email: ".$email , "From: webhobby@maxhooktravelblog.ru \r\n");
 ?>
+
+
