904c6d122b2a710b92fc88850b4c7d9564396844
Maxim Utkin <maxim2001_2001@mail.ru>
2021-02-24 22:56:12 +0300

    Рабочая форма отправки заявки

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,7 @@
 $('#order').submit(function(){
     var formData=$('#order').serialize();
     $.post('order.php', formData);
+  alert("Заявка успешно отправлена!");
+  document.getElementById('order').reset(); //очищаем данные формы после отправки
+  return false; // предотвращаем переход на страницу order.php
 }); // конец submit
